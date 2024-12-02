56a6ae93d5f3fff9beedaeb8427d9055dfdda3a9
sandthecool12 <144626385+sandthecool12@users.noreply.github.com>
2024-12-02 18:20:09 -0500

    DONE!

--- 2024/day2-work/pt2.py
+++ 2024/day2-work/pt2.py
@@ -1,6 +1,6 @@
 safeCounter=0
-badOnes = []
 with open('input.txt','r') as file:
+    # Go through each line and see if we 
     for lines in file:
         numList = lines.replace('\n','').split()
         def numberChecker(x):
@@ -10,25 +10,18 @@ with open('input.txt','r') as file:
                 if i == 0:
                     if (int(x[1])-int(x[0]))<0:
                         isIncreasing = False
-                    else:
-                        isIncreasing = True
                 else:
-                    eval = int(num) - int(numList[i-1])
+                    eval = int(num) - int(x[i-1])
                     if(isIncreasing and (eval<=0  or eval>3))or(not isIncreasing and (eval>=0 or eval<-3)):
                         isSafe = False
             return isSafe
         if numberChecker(numList):
             safeCounter+=1
         else:
-            transfer = []
-            for num in numList:
-                transfer.append(int(num))
-            badOnes.append(transfer)
-
-
-        
-            
-            
-
-
+            for x in range(len(numList)):
+                newNumList=numList.copy()
+                newNumList.pop(x)
+                if numberChecker(newNumList):
+                    safeCounter+=1
+                    break
 print(safeCounter)
