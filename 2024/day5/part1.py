347fc12f0b9e0eb497e12f205eaeb89c8f98a510
Sanders <coolmansand@gmail.com>
2024-12-04 07:02:25 -0500

    set up part 1

---
+++ 2024/day5/part1.py
@@ -0,0 +1,45 @@
+plus3 = []
+plus2 = []
+plus1 =[]
+# Check for diagonal and Vertical XMAS's
+mid = []
+neg1 = []
+neg2 = []
+# Check for horazantal XMAS's
+neg3 = []
+
+
+# x--x--x
+# -x-x-x-
+# --xxx--
+# ---o---
+# --xxx--
+# -x-x-x-
+# x--x--x
+
+
+
+with open('day5\example.txt') as txt:
+    def checkHor (x):
+        print (x)
+        
+        
+    def checkVert(x):
+        # plus3 = 0,3,6
+        # plus2 = 1,3,5
+        # plus1 = 2,3,4
+        # mid = 3
+        # neg1= 2,3,4
+        # neg2 = 1,3,5
+        # negs3 = 0,3,6
+        print(x)
+
+    def checkDi(x):
+        print(x)
+
+    
+    
+    for line in txt:
+        row = line.replace('\n',"")
+        print(row)
+
